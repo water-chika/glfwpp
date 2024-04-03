b393f49d3770a549fcb16cb9fbfdf96aef133581
Water Chika <water_chika@outlook.com>
2024-04-04 02:48:18 +0800

    Add some wrapper functions.

--- glfwpp.cpp
+++ glfwpp.cpp
@@ -1,2 +1,41 @@
-#include <GLFW/glfw3.h>
+#include "glfwpp.hpp"
+#include <cassert>
 
+// used for Debug.
+static bool is_inited = false;
+
+bool glfwpp::init() {
+	assert(!is_inited);
+	is_inited = glfwInit();
+	return is_inited;
+}
+
+void glfwpp::terminate() {
+	assert(is_inited);
+	is_inited = false;
+	glfwTerminate();
+}
+
+void glfwpp::window_hint(int hint, int value) {
+	glfwWindowHint(hint, value);
+}
+
+GLFWwindow* glfwpp::create_window(int width, int height, const char* title, GLFWmonitor* monitor, GLFWwindow* share) {
+	return glfwCreateWindow(width, height, title, monitor, share);
+}
+
+void glfwpp::destroy_window(GLFWwindow* window) {
+	glfwDestroyWindow(window);
+}
+
+VkResult glfwpp::create_surface(VkInstance instance, GLFWwindow* window, const VkAllocationCallbacks* allocator, VkSurfaceKHR* surface) {
+	return glfwCreateWindowSurface(instance, window, allocator, surface);
+}
+
+bool glfwpp::window_should_close(GLFWwindow* window) {
+	return glfwWindowShouldClose(window);
+}
+
+void glfwpp::poll_events() {
+	glfwPollEvents();
+}
--- glfwpp.hpp
+++ glfwpp.hpp
@@ -1,3 +1,31 @@
-namespace glfwpp {
+#include <vulkan/vulkan.h>
+#include <GLFW/glfw3.h>
+#include <cassert>
 
+namespace glfwpp {
+	bool init();
+	void terminate();
+	void window_hint(int hint, int value);
+	GLFWwindow* create_window(int width, int height, const char* title, GLFWmonitor* monitor, GLFWwindow* share);
+	void destroy_window(GLFWwindow* window);
+	VkResult create_surface(VkInstance instance, GLFWwindow* window, const VkAllocationCallbacks* allocator, VkSurfaceKHR* surface);
+	bool window_should_close(GLFWwindow* window);
+	void poll_events();
+	class window {
+	public:
+		void create(int width, int height, const char* title, GLFWmonitor* monitor, GLFWwindow* share) {
+			m_window = create_window(width, height, title, monitor, share);
+		}
+		void destroy() {
+			destroy_window(m_window);
+		}
+		bool should_close() {
+			return window_should_close(m_window);
+		}
+		operator GLFWwindow* () {
+			return m_window;
+		}
+	private:
+		GLFWwindow* m_window;
+	};
 }
